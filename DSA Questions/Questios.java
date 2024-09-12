55f914e248e43a9bec8d23a6700c28ee162b0812
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-12 23:03:00 +0530

    hiii

---
+++ DSA Questions/Questios.java
@@ -0,0 +1,15 @@
+// package DSA Questions;
+import java.util.Scanner;
+
+public class Questios 
+{
+    public static void main(String[] args) 
+        {
+            Scanner input = new Scanner(System.in);
+            int marks;
+            System.out.println("Enter marks ");
+            marks = input.nextInt();
+            System.out.println("Your marks is " + marks);
+        }
+}
+
