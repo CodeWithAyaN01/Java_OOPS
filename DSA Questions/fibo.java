735c8d68c92fc2afa890122983cfc2b01ca0f1a2
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-14 19:49:38 +0530

    fibonaci

---
+++ DSA Questions/fibo.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class fibo
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to my Program for Fibonaci ");
+        Scanner sc = new Scanner(System.in);
+        //  to make fibonaci 
+
+        int a = 0 ; int b = 1 ;
+        int count = 0;
+        int num;
+        int temp;
+
+        System.out.println("Enter the number till you wnat fibnaci");
+
+        num = sc.nextInt();
+
+        for (int i = 0 ; i<num ; i++)
+        {
+            System.out.print(a);
+            temp = b;
+            a = a+b;
+            b = a;
+            a = temp;            
+        }
+    }
+}
---
+++ DSA Questions/pandey.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pandey {
+    
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String user = sc.next();
+        System.out.println(user);
+        int length = user.length();
+    
+
+
+
+    }
+}
