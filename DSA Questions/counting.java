4b3ec7ce5c3520f1e259c58b976a65ddba41a8e2
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-15 15:53:34 +0530

    Done by maths

--- DSA Questions/counting.java
+++ DSA Questions/counting.java
@@ -1,38 +1,66 @@
+// import java.util.Scanner;
+// public class counting {
+//     public static void main(String[] args) {
+        
+//         // to count the same give occurring number //
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter number : ");
+//         long number = sc.nextInt();
+//         String str;
+        
+//         str = String.valueOf(number);
+        
+//         int len = str.length();
+//         int count=0;
+        
+//         System.out.println("Enter number you wnat to count : ");
+//         int num = sc.nextInt();
+//         String charr = String.valueOf(num);
+
+//         for (int i = 0 ; i<len ; i++)
+//         {
+//             if (str.charAt(i) == charr.charAt(0))
+//             {
+//                 count++; 
+//             }
+
+//         }
+//         System.out.println("The number occurs : "+ count + " Times");
+
+//     }
+// }
+
+
+//////////////////////// by % and by math approach/////////////////////
+
 import java.util.Scanner;
-public class counting {
+
+public class  counting
+{
     public static void main(String[] args) {
         
-        // to count the same give occurring number //
+        Scanner input = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number : ");
-        long number = sc.nextInt();
-        String str;
-        
-        str = String.valueOf(number);
-        
-        int len = str.length();
-        int count=0;
-        
-        System.out.println("Enter number you wnat to count : ");
-        int num = sc.nextInt();
-        String charr = String.valueOf(num);
+        System.out.println("Enter the number");
+        long number = input.nextInt();
 
+        System.out.println("Enter the number you wnat to find: ");
+        int num = input.nextInt();
+
+        int len = (String.valueOf(number)).length();
+
+        int counting = 0;
+        System.out.println(number);
         for (int i = 0 ; i<len ; i++)
         {
-            if (str.charAt(i) == charr.charAt(0))
+            if (number % 10 == num)
             {
-                count++; 
+                counting++;
             }
-
+            number = number/10;
         }
-        System.out.println("The number occurs : "+ count + " Times");
-
-
-      
-
-
-
+        System.out.println("The number \'" + num + "\' is found " + counting + " Times");
 
 
     }
