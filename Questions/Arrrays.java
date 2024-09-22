32b81b9c8abf2b2a4bae36d9d0ce5a1303fd12e5
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-22 16:42:34 +0530

    learning Arrays

---
+++ Questions/Arrrays.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Arrrays
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome");
+        Scanner sc = new Scanner(System.in);
+        String[] name = new String[4];
+        for (int i = 0; i < name.length ; i++){ //  only length not length()
+
+            name[i] = sc.next();
+        }
+        System.out.println(Arrays.toString(name));
+    }
+}
--- Questions/try.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class try {
-    public static void main(String[] args) {
-        // trying to store a string values in arrays 
-
-    Scanner sc = new Scanner(System.in);
-
-    }
-}
