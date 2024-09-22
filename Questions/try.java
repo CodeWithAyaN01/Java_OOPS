101ee7ee1c35580f711ff691b14148dde8cd3e06
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-22 14:55:51 +0530

    sync

---
+++ Questions/try.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class try {
+    public static void main(String[] args) {
+        // trying to store a string values in arrays 
+
+    Scanner sc = new Scanner(System.in);
+
+    }
+}
