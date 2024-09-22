c87c75ea753fc3c5adc4e6f522cae4ea5c1e3374
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-22 20:51:52 +0530

    i

--- Questions/Arrrays.java
+++ Questions/Arrrays.java
@@ -8,7 +8,7 @@ public class Arrrays
         Scanner sc = new Scanner(System.in);
         String[] name = new String[4];
         for (int i = 0; i < name.length ; i++){ //  only length not length()
-            System.out.println("Enter name " + (i+1) );
+            System.out.println("Enter name " + (i+1));
             name[i] = sc.next();
         }
         System.out.println(Arrays.toString(name));
