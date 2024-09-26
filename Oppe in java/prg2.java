5b3992c38b8efce0847f4b2a36e6f3b2752d50dc
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-26 23:40:45 +0530

    pending

---
+++ Oppe in java/prg2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Array;
+
+class Mentoring_for_Student_IS
+{   
+    int usn;
+    String StudentName;
+
+    void Display()
+    {
+        System.out.println("********************************************************************");
+        System.out.println("                Welcome to Mentoring System");
+        System.out.println("********************************************************************");
+
+        System.out.print("The student name is : " + StudentName);
+        System.out.print("USN of " + StudentName + " is: " + usn);
+        System.out.println("end");
+    }
+}
+public class prg2
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] rolls = new int[100];
+        System.out.println("How many student details you want to add: ");
+        int countStd = nextInt();
+        for (int i = 0 ; i<countStd ; i++)
+        {
+            
+        }
+        
+    }
+}
