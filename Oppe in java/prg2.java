30f021fb0d68980667d480cd9132f55bec21bf49
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-28 12:08:51 +0530

    Partially working

--- Oppe in java/prg2.java
+++ Oppe in java/prg2.java
@@ -7,28 +7,44 @@ class Mentoring_for_Student_IS
     // THIS IS A UNPARAMETERIZED CONSTRUCTER // 
     Mentoring_for_Student_IS() {
         usn = 0;
-        StudentName = "NULL";
+        StudentName = "unknown";
     }
 
     // dispaly function 
     void Display()
     {
-        System.out.println("********************************************************************");
-        System.out.println("                Welcome to Mentoring System");
-        System.out.println("********************************************************************");
+        // System.out.println("********************************************************************");
+        // System.out.println("                Welcome to Mentoring System");
+        // System.out.println("********************************************************************");
 
-        System.out.print("The student name is : " + StudentName);
+        System.out.println("The student name is"+ StudentName);
         System.out.print("USN of " + StudentName + " is: " + usn);
-        System.out.println("end");
+        // System.out.println("end");
     }
 }
 public class prg2
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Mentoring_for_Student_IS s1 = new Mentoring_for_Student_IS();
-        s1.usn = 037;
-        s1.StudentName = "Ayna";
-        s1.Display();
+        System.out.print("Enter how many Student are there: ");
+        int n = sc.nextInt();
+        Mentoring_for_Student_IS[] stdInfo = new Mentoring_for_Student_IS[n]; // making array of student[] of n size and askin n from user
+        
+        for(int i = 0 ; i<n ; i ++)
+        {
+            stdInfo[i] = new Mentoring_for_Student_IS(); //this is to initilizse a object;
+            System.out.println("Enter details of Student "+(i+1));
+            System.out.print("Enter the usn of Student:");
+            stdInfo[i].usn = sc.nextInt();
+            sc.nextLine(); // commenting this line shows you error; 
+            System.out.print("Enter the nane Of student: ");
+            stdInfo[i].StudentName = sc.nextLine();
+        }
+
+        for (int a = 0 ; a<n ; a++)
+        {
+            stdInfo[a].Display();
+            System.out.println("\n");
+        }
     }
 }
