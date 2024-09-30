242db46dd50f36bd8777b8ed6ba03d142c97a49d
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-09-30 17:17:23 +0530

    remake

--- Questions/Q2.java
+++ Questions/Q2.java
@@ -18,7 +18,7 @@ public class Q2 {
         int count = 0;
         for (int i = 0 ; i<list.length ; i++)
         {
-            for (int j = 0 ; j<list.length ; j++)
+            for (int j = i+1 ; j<list.length ; j++) // issue !! (j = 0)
             {   
                 if(list[i] + list[j] == k)
                 {
