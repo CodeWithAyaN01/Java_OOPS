6b6c21360043f8f93d4e9c12a6ca896d3517e507
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-09 02:14:41 +0530

    Methods;

---
+++ Module 2/classEx.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+class Automobiles
+{
+    int number;
+    String name;
+}
+public class classEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        Automobiles car1 = new Automobiles();
+        
+    }
+}
---
+++ Module 2/classEx2.java
@@ -0,0 +1,9 @@
+public class classEx2 {
+    public static void sayHello() // methods without classes ;
+    {
+        System.out.println("Hello Darling");
+    }
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
