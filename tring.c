4d760f2cb1547f6983b022bfec84beaaafad80c4
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-11 15:57:39 +0530

    sgructures

--- tring.c
+++ tring.c
@@ -1,23 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
-int increseSalary(int CurrAmount , int Percentage)
-{
-    int inc = CurrAmount + (((CurrAmount*Percentage))/100);
-    
+
+// Define a structure for Employee
+struct Employee {
+    char EmpName[20];
+    int CurrAmount;
+    int IncrementInPercentage;
+};
+
+// Function to increase salary
+int increaseSalary(struct Employee emp) {
+    int inc = emp.CurrAmount + ((emp.CurrAmount * emp.IncrementInPercentage) / 100);
     return inc;
 }
-int main()
-{
-    printf("Welcome to My company sal increment System:\n ");
-    char EmpName[20];
-    int IncrementInPercentage,amount;
-    printf("Enter details of the employee: \n");
-    printf("Enter the name of Employee: ");gets(EmpName);
-    printf("Enter the current amount\n");scanf("%d",&amount);
-    printf("Enter the percentage : ");scanf("%d",&IncrementInPercentage);
 
-    int a = increseSalary(amount,IncrementInPercentage);
+int main() {
+    struct Employee emp;  // Create a variable of the Employee structure
 
-    printf("The new amount of %s with a percent of %d is:  %d",EmpName,IncrementInPercentage,a);
+    printf("Welcome to My Company Salary Increment System:\n");
 
-}
+    // Get employee details
+    printf("Enter the name of Employee: ");
+    gets(emp.EmpName);
+    printf("Enter the current amount: ");
+    scanf("%d", &emp.CurrAmount);
+    printf("Enter the percentage increment: ");
+    scanf("%d", &emp.IncrementInPercentage);
+
+    // Calculate new salary
+    int newAmount = increaseSalary(emp);
+
+    // Display the result
+    printf("The new amount for %s with a percentage increment of %d%% is: %d\n", 
+           emp.EmpName, emp.IncrementInPercentage,
