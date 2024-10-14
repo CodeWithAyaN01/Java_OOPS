df6cb0d406c106cbc048ab4577b933d93fa349f5
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-14 19:11:07 +0530

    commit

--- Questions/ArrayListtt.java
+++ Questions/ArrayListtt.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 public class ArrayListtt {
     public static void main(String[] args) {
         ArrayList<Integer> main = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,6,7,8)); //  adding the elem in the decleration it-self //  need to import Array for this
-        main.add(5);
-        main.set(1,100);
-        main.remove(1);
-        System.out.println((main));
-
+        main.add(5); //  adding the elem at the last !
+        main.set(1,100); //  setting the element of the index .set(index,value)
+        main.remove(1); // remove the elem from the index 
+        main.remove(Integer.valueOf(8)); // removes the value 8 not the index of 8
+         System.out.println((main));
     }
 }
