5acffdd10a15ccca953e43dbe44d5001d40a1a45
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-17 14:29:10 +0530

    example

--- String Builder/basic.java
+++ String Builder/basic.java
@@ -8,5 +8,16 @@ public class basic {
         name2 = "Ayan Only";
         System.out.println(name2); //  name 2 creates new object and it pointes to new object
         
+        //  CHECKING IF BOTH VARIABLES ARE POINTING TO SAME VARIABLE OR NOT  //
+
+        System.out.println(name == name2); // returs "false" beacuse name2 was changed
+        
+        // CREATE A NEW OBJECT IN //
+
+        String a = "hello"; //  in this case both a and b are pointing to same "hello" 
+        String b = "hello";
+        System.out.println(a == b); // true
+        String c = new String("hello"); //  this creates new object of  hello so object of a and b are not same as object of c;
+        System.out.println( a == b && b == c); // false
     }
 }
