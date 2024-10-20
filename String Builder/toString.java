7734305bce671382ad57fd005ea7f9c934664ccb
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-20 16:54:10 +0530

    toStringMethod

---
+++ String Builder/toString.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class toString
+{
+    public static void main(String[] args) 
+    {
+        int[] a = {1,2,3,4,5};
+        System.out.println(Arrays.toString(a)); // we can use Arrays.toString() to print arrays;
+    }
+}
