c8ff9831f1b0bccb14c5a7ff8ab780ca9df4465b
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-12-04 19:21:22 +0530

    Done

--- Testing.java
+++ Testing.java
@@ -1,9 +1,70 @@
-import folder.Main;
+import java.util.Arrays;
+import java.util.Scanner;
 public class Testing {
+    static Scanner sc = new Scanner(System.in);
+    static int rows;
+    static int col;
+    static char[][] a = new char[10][10];
+    static char[][] b = new char[10][10];
+    static int[] R = new int[rows+1];
+    static int[] C = new int[col+1];
+
+    static void TakeInput()
+    {   
+        System.out.print("Enter number of rows: ");
+        rows = sc.nextInt();
+        System.out.print("Enter the numebr of Col: ");
+        col = sc.nextInt();
+        System.out.print("Start Entering the Matrix: \n");
+        for(int i = 0 ; i < rows ; i++)
+        {
+            for(int j = 0 ; j < col ; j++)
+            {
+                a[i][j] = sc.next().charAt(0);
+            }
+        }
+    }
+    static void DisplayMatrix(char[][] mat) {
+        System.out.println("The matrix is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        Main obj = new Main();
-        obj.Display();
-        // obj.add(4,5);
-        System.out.println("Add is "+obj.add(4,5));
+        int q = 0;
+        int r = 0;
+        TakeInput();
+        for (int i = 0 ; i<rows ; i++)
+        {
+            for (int j = 0 ; j < col ; j++)
+            {
+                if (a[i][j] == '0')
+                {
+                    R[q++] = i;
+                    C[r++] = j;
+                }
+            }
+        }
+
+        System.out.println("Rows are : "+Arrays.toString(R));
+        System.out.println("col are: "+Arrays.toString(C));
+        for (int i = 0 ; i<col ; i++)
+        {
+            int c= 0;
+            a[R[c]][i] = '0';
+            c++;
+        }
+        for (int j = 0 ; j<rows ; j++)
+        {
+            int k = 0;
+            a[j][C[k]] = '0';
+        }
+        DisplayMatrix(a);
     }
 }
+
+
+
--- folder/Main.java
+++ folder/Main.java
@@ -1,12 +1,82 @@
-package folder;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
-    
-    public static void Display()
-    {
-        System.out.println("\nHello fron folder package");
+    static Scanner sc = new Scanner(System.in);
+    static int rows;
+    static int col;
+    static char[][] a = new char[10][10];
+    static char[][] b = new char[10][10];
+    static ArrayList<Integer> R = new ArrayList<>();
+    static ArrayList<Integer> C = new ArrayList<>();
+    static int num = 0;
+    static int c= 0;
+    static int k= 0;
+
+
+    static void TakeInput()
+    {   
+        System.out.print("Enter number of rows: ");
+        rows = sc.nextInt();
+        System.out.print("Enter the numebr of Col: ");
+        col = sc.nextInt();
+        System.out.print("Start Entering the Matrix: \n");
+        for(int i = 0 ; i < rows ; i++)
+        {
+            for(int j = 0 ; j < col ; j++)
+            {
+                a[i][j] = sc.next().charAt(0);
+            }
+        }
     }
-    public static int add(int num1,int num2)
-    {
-        return num1+num2;
+    static void DisplayMatrix(char[][] mat) {
+        System.out.println("The matrix is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int q = 0;
+        int r = 0;
+        TakeInput();
+        for (int i = 0 ; i<rows ; i++)
+        {
+            for (int j = 0 ; j < col ; j++)
+            {
+                if (a[i][j] == '0')
+                {
+                    // R[q++] = i;
+                    // C[r++] = j;
+                    R.add(i);
+                    C.add(j);
+                }
+            }
+        }
+
+        // System.out.println("Rows are : "+R); 
+        for (int c = 0 ; c < R.size() ; c++)
+        {
+            int row = R.get(c);
+            for (int f = 0 ; f<col ; f++)
+            {
+                a[row][f] = '0';
+            }
+        }
+        
+        for (int c = 0 ; c < C.size() ; c++)
+        {
+            int coln = C.get(c);
+            for (int f = 0 ; f<rows ; f++)
+            {
+                a[f][coln] = '0';
+            }
+        }
+        DisplayMatrix(a);
     }
 }
+
+
+
