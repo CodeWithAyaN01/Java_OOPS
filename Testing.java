157cf67ddfc175c05401ff96337a5c76b4d3f254
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-12-04 10:44:24 +0530

    Done

---
+++ Testing.java
@@ -0,0 +1,9 @@
+import folder.Main;
+public class Testing {
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.Display();
+        // obj.add(4,5);
+        System.out.println("Add is "+obj.add(4,5));
+    }
+}
---
+++ folder/Main.java
@@ -0,0 +1,12 @@
+package folder;
+public class Main {
+    
+    public static void Display()
+    {
+        System.out.println("\nHello fron folder package");
+    }
+    public static int add(int num1,int num2)
+    {
+        return num1+num2;
+    }
+}
