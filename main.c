072eb44b02dc910c3210638ff98910803761e664
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-12-20 08:32:49 +0530

    Done

--- main.c
+++ main.c
@@ -1,38 +1,124 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    int count = 0;
-    int arr[] = {0,0,0,1,5,7,0,0};
-    int len = 8;
-    // int assig = 7;
-    // [1,3,2,5,4,0,0]
-    for (int i = 0 ; i<len ;i++)
-    {
-        if (arr[i] == 0)
-        {
-            count++;
-        }
-    }
-    for (int i = 0 ; i< len ; i++ )
-    {
-        int j;
-        if (arr[i] == 0)
-        {
-            arr[i] = arr[i+1];
-            for (int k = i ; k< len-count+1 ; k++)
-                arr[k] = arr[k+1];
-        }
-    }
-    for (int h = 0 ; h <count; h++)
-    {
-        arr[len-count+h] = 0;
-    }
-    // arr[len-count] = 0;
-    // arr[len-count+1] = 0;
-    for (int i = 0 ; i< len ; i++ )
-    {
-        printf("%d\t",arr[i]);
-    }
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include<windows.h>
+#include <stdbool.h>
+#include <unistd.h>
+#define max 15
+int q[max];
+int rear=-1,front=0;
 
+typedef struct{
+	int counterno;
+	int customerno;
+	int wt;
+	struct counter*next;
+}counter;
+
+counter *head=NULL;
+
+pthread_mutex_t qm = PTHREAD_MUTEX_INITIALIZER;
+
+counter *create(){
+	counter new1=(counter)malloc(sizeof(counter));
+	printf("Enter the counter number and counter's waiting time:");
+	scanf("%d%d",&new1->counterno,&new1->wt);
+	new1->next=NULL;
+	return new1;
+}
+
+bool isfull(){
+	return rear==max-1;
+}
+bool isempty(){
+	return rear<front;
+}
+void enqueue(int data){
+	if(isfull()) return;
+	rear=rear+1;
+	q[rear]=data;
+}
+int dequeue(){
+	if(isempty()) return -1;
+	int data=q[front];
+	front=front+1;
+	return data;
+}
+void createqueue(){
+	int i;
+	for(i=1;i<=max;i++)
+		enqueue(i);
+}
+
+
+void createlist(int n) {
+    counter *newCounter, *temp;
+    int i;
+    for (i = 1; i <= n; i++) {
+        newCounter = create(); 
+        if (head == NULL) {
+            head = newCounter; 
+            newCounter->next = head;
+        } else {
+            temp = head;
+            while (temp->next != head) {
+                temp = temp->next;
+            }
+            temp->next = newCounter; 
+            newCounter->next = head; 
+        }
+    }
+}
+
+//void assigncounter(){
+//	counter *temp=head;
+//	while(!isempty()){
+//		temp->customerno=dequeue();
+//		printf("\ncounter no %d has been allocated to customer no %d",temp->counterno,temp->customerno);
+//		temp=temp->next;
+//	}
+//}
+
+void *counter_function(void *arg){
+	counter *ctr=(counter *)arg;
+	while(1){
+		pthread_mutex_lock(&qm);
+		if(isempty()){
+			pthread_mutex_unlock(&qm);
+			break;
+		}
+		ctr->customerno=dequeue();
+		pthread_mutex_unlock(&qm);
+		printf("\nCounter %d is serving customer %d for %d seconds.",ctr->counterno,ctr->customerno,ctr->wt);
+		sleep(ctr->wt);
+		printf("\nCounter %d finished serving customer %d.",ctr->counterno,ctr->customerno);
+	}
+	return NULL;
+}
+//
+//void visual(){
+//	printf("-------------------------------------------------------------------------------------------------------------------------------------------------\n");
+//	printf("|\t\tCounter 1\t\t\t|\t\t\tCounter 2\t\t\t|\t\t\tCounter 3\t\t|\n");
+//	printf("-------------------------------------------------------------------------------------------------------------------------------------------------\n");
+//}
+
+int main(){
+	int i,n;
+	printf("Enter the number of counters:");
+	scanf("%d",&n);
+	createlist(n);
+	createqueue();
+	visual();
+	pthread_t t[n];
+	counter *temp=head;
+	for(i=0;i<n;i++){
+		pthread_create(&t[i],NULL,counter_function,(void *)temp);
+		temp=temp->next;
+	}
+	for(i=0;i<n;i++){
+		pthread_join(t[i],NULL);
+	}
+	printf("\nAll customers have been served.");
+	//assigncounter();
+	return 0;
 }
