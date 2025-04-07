905a15ac8118003e6f92053114e706e7711296c3
CodeWithAyaN01 <ayankrrish@gmail.com>
2025-04-08 02:35:33 +0530

    Done

--- Advance Java/prg6.java
+++ Advance Java/prg6.java
@@ -8,31 +8,24 @@ public class prg6
         System.out.println("capacity is :" + sb.capacity());
 
         sb.setLength(10);
-
         System.out.println("Set length to 10: "+sb);
 
         sb.delete(5,10);
-
         System.out.println("Delete From index 5 to 10"+ sb);
 
         sb.reverse();
-
         System.out.println("Reverse the Sb : "+ sb);
 
         sb.append("good bye");
-
         System.out.println("New Sb:"+sb);
 
         sb.insert(6,"hello");
-
         System.out.println("insert hello at 6:"+sb);
 
         sb.deleteCharAt(5);
-
         System.out.println("Deleted cg=hat : "+ sb);
 
         sb.replace(0, 3, "hii");
-
         System.out.println("---."+ sb);
 
 
