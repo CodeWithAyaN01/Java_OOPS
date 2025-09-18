38fdbee17213bf57cbe3a408cefbd0b64575b7f3
CodeWithAyaN01 <ayankrrish@gmail.com>
2025-09-19 03:42:07 +0530

    Add insertAtK method to LinkedList for inserting a node at a specific position

--- Linked-List/insertAtHead.java
+++ Linked-List/insertAtHead.java
@@ -24,8 +24,7 @@ public class insertAtHead {
         head = temp;
         return head;
     }
-
-    Node insertAttail(Node head ,int value)
+    Node insertAttail(Node head ,int value) // some error 
     {
         if(head == null || head.next == null){
             return null;
@@ -40,6 +39,31 @@ public class insertAtHead {
         temp.next = elem;
         return head;
     }
+    Node insertAtK(Node head,int data,int k)
+    {   
+        if(k<=0)
+        {
+            return head;
+        }
+        // so k varies from 1 to n-1 positions
+        if(k==1)
+        {
+            return insertAtHead(head, data); // edge case
+        }
+        Node temp = head; // for iteration
+        int count =1; // counter variable
+        while (temp != null && count<k-1){ // this stops at the just befor the kth element eg: k ==3 stops at k ==2 and links them
+            temp = temp.next;
+            count++;
+        }
+        if (temp == null) { // if the given k value is greater than length
+            return head;
+        }
+        Node n = new Node(data,temp.next);
+        temp.next = n;
+
+        return head;
+    }
     public static void main(String[] args) {
         Node head = new Node(1);
         Node n1 = new Node(2);
@@ -47,7 +71,7 @@ public class insertAtHead {
         Node n3 = new Node(4);
         Node n4 = new Node(5);
     
-        head.next = n1;
+        head.next = n1; // connection of each node
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
@@ -55,6 +79,7 @@ public class insertAtHead {
         insertAtHead obj = new insertAtHead();
         head = obj.insertAtHead(head,0); //  inserted 0 at the head
         head = obj.insertAttail(head, 6); //  inserted 6 at tail of
+        head = obj.insertAtK(head, 69, 0); //  insert at k number
         Node temp = head;
 
         while(temp!=null)
